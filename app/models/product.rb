b61408390c8f5d926e4692a5e39d8eda626d3d1c
lucascr <lcrodrigues1995@gmail.com>
2023-03-08 16:16:15 -0300

    Do relations and validations for the products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ApplicationRecord
+  belongs_to :category
+  has_many :categories
+  belongs_to :inventory
+  has_many :inventories
+
+  validates :price, :name, :description, :inventory_id, :category_id
 end
