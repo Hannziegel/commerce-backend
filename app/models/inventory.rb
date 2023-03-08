5e23a971449d48db530bdc311c6f1fdbbe885adc
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2023-03-08 15:17:35 -0300

    Add validations and update model Inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,8 @@
 class Inventory < ApplicationRecord
   belongs_to :address
   belongs_to :product
+  has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
+  validates :product_id, :quantity, :order_id, :address_id
 end
