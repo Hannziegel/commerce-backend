339d78c10ce403942a2843bc276ed40c5898c039
Rodrigo J. R. Nunes <hannziegel@gmail.com>
2023-03-08 15:15:20 -0300

    Add validations and update model Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,7 @@
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :inventory
+  has_many :products, through: :inventory, dependent: :destroy
+
+  validates :inventory_product_id, :quantity, :user_id
 end
