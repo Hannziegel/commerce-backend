61f5ba5f7e713927e6d49ed32bd6d0c4897aad96
lucascr <lcrodrigues1995@gmail.com>
2023-03-08 16:16:01 -0300

    Do relations and validations for the Orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
+  belongs_to :inventory
+  has_many :inventories
+  
+  validates :quantity, :buy_date, :confirmation_date, :delivered, :user_id, :inventory_id
 end
